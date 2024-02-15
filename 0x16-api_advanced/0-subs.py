3bfb953feac03bc11736076094fb9b25a93cb710
Mafika <rudolphmafika@gmail.com>
2024-02-15 02:25:41 -0800

    deleted file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ contains a function that return the number of subcribers from reddit API
 import requests
 
 def number_of_subscribers(subreddit):
-    
+     
     #read credentials
     file_path = 'credentials.txt'
     credentials = []
@@ -21,9 +21,9 @@ def number_of_subscribers(subreddit):
     SECRET_KEY = credentials[0][1]
     username = credentials[1][0]
     password = credentials[1][1]
-
+    
     headers = {"User-Agent": "0-subs/1.0"}
-
+    
     auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
     data = {
             "grant_type": "password",
@@ -34,6 +34,7 @@ def number_of_subscribers(subreddit):
     res = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
     Token = res.json()["access_token"]
     headers["Authorization"] = f"bearer {Token}"
+    
 
     response = requests.get(f"https://oauth.reddit.com/r/{subreddit}/about.json", headers=headers)
     if (response.status_code == 200):
